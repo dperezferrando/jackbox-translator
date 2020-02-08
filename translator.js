4ef4b69e1f7d4f86c3d711b322ea0e4ced05d32b
Diego <diegoperezferrando@gmail.com>
2020-02-07 23:19:33 -0300

    Reemplaza en los archivos del juego

--- translator.js
+++ translator.js
@@ -1,19 +1,39 @@
 import Promise from "bluebird";
 import _ from "lodash";
 import highland from "highland";
+import hash from "string-hash";
 
 const fs = Promise.promisifyAll(require("fs"));
 const PATH = "I:\Juegos\\Steam\\steamapps\\common\\The Jackbox Party Pack 6\\games\\PushTheButton\\content"
 const FOLDERS = ["PushTheButtonDrawingTests", "PushTheButtonMoralityTests", "PushTheButtonRatingTests", "PushTheButtonWritingTests"]
 const LOCALIZATION = {}
 
+const isPrompt = (property) => /Text/gi.test(property) || /HumanPromptAudio/gi.test(property) 
+
+const replaceWithIdIfPromp = (field, dir) => {
+  if(!isPrompt(field.n))
+    return field;
+  if(field.s)
+    return { ...field, s: `TRANSLATION_${dir}_${hash(field.s)}`}
+  return { ...field, v: `TRANSLATION_${dir}_${hash(field.v)}`}
+}
+
+const modifyFile = (file, fullPath, dir) => {
+  const { fields } = file;
+  const newFields = fields.map(it => replaceWithIdIfPromp(it, dir));
+  return fs.writeFileAsync(fullPath, JSON.stringify({ ...file,  fields: newFields }));
+
+}
+
 const readAndProcessDataFile = (path, dir) => {
   const fullPath = `${path}\\${dir}\\data.jet`;
   return highland(fs.readFileAsync(fullPath))
-    .map(it => JSON.parse(it.toString()).fields)
+    .map(it => JSON.parse(it.toString()))
+    .tap(it => modifyFile(it, fullPath, dir))
+    .map(it => it.fields)
     .sequence()
-    .filter(({ n }) => /Text/gi.test(n))
-    .tap(({v}) => _.assign(LOCALIZATION, { [`TRANSLATION_${dir}`]: v }))
+    .filter(({ n }) => isPrompt(n))
+    .tap(({ v, n, s }) =>  _.assign(LOCALIZATION, { [`TRANSLATION_${dir}_${hash(s ? s: v)}`]: s ? s: v }))
 }
 
 const processFolder = (folderName) => {
@@ -26,4 +46,4 @@ const processFolder = (folderName) => {
     .then(() => fs.writeFileAsync("./localization.json", JSON.stringify(LOCALIZATION)))
 }
 
-Promise.map(_.take(FOLDERS, 1), processFolder,{concurrency: 2})
+Promise.map(_.take(FOLDERS, 2), processFolder,{concurrency: 2})

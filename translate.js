f564de3173fcdabfe8a533ff4bd0e0034f4714b8
Diego <diegoperezferrando@gmail.com>
2020-03-24 20:40:55 -0300

    fix route

--- translate.js
+++ translate.js
@@ -1,6 +1,6 @@
 import JackboxTranslator from "./domain/jackboxTranslator";
 import pushTheButton from "./domain/games/pushTheButton"
-import * as localization from "../localization"
+import * as localization from "./localization"
 
 new JackboxTranslator("I:\Juegos\\Steam\\steamapps\\common", pushTheButton, localization)
   .run()

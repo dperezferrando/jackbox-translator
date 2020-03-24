c5ef6f460319fcf8adbd95d24156791cb090cf48
Diego <diegoperezferrando@gmail.com>
2020-03-24 20:18:11 -0300

    Test de translator

--- domain/jackboxTranslator.js
+++ domain/jackboxTranslator.js
@@ -1,13 +1,18 @@
 import hash from "string-hash";
 import _ from "lodash";
 import JackboxProcessor from "./jackboxProcessor";
-import * as localize from "../localization"
 
 class JackboxTranslator extends JackboxProcessor {
 
+  constructor(steamPath, game, localization) {
+    super(steamPath, game);
+    this.localization = localization
+  }
+
+
   _modificator_ (field, property) {
 
-    return { ...field, [property]: localize[field[property]] };
+    return { ...field, [property]: this.localization[field[property]] };
   }
 
 }
--- translate.js
+++ translate.js
@@ -1,6 +1,7 @@
 import JackboxTranslator from "./domain/jackboxTranslator";
 import pushTheButton from "./domain/games/pushTheButton"
+import * as localization from "../localization"
 
-new JackboxTranslator("I:\Juegos\\Steam\\steamapps\\common", pushTheButton)
+new JackboxTranslator("I:\Juegos\\Steam\\steamapps\\common", pushTheButton, localization)
   .run()
 

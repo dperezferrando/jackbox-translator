77df520c46a83f781ddf358805c4b0d722a779bc
Diego <diegoperezferrando@gmail.com>
2020-03-24 19:50:15 -0300

    Borro lo viejo

--- generateLocalization.js
+++
@@ -1,7 +0,0 @@
-import Promise from "bluebird";
-import JackboxLocalizator from "./domain/jackboxLocalizator";
-import pushTheButton from "./domain/games/pushTheButton"
-
-new JackboxLocalizator("I:\Juegos\\Steam\\steamapps\\common", pushTheButton)
-  .run()
-
--- localize.js
+++ localize.js
@@ -1,40 +1,7 @@
 import Promise from "bluebird";
-import _ from "lodash";
-import highland from "highland";
-import hash from "string-hash";
-import { isPrompt, modifyFile, processFolder, TEXT_PROPERTIES } from "./common";
+import JackboxLocalizator from "./domain/jackboxLocalizator";
+import pushTheButton from "./domain/games/pushTheButton"
 
-const fs = Promise.promisifyAll(require("fs"));
+new JackboxLocalizator("I:\Juegos\\Steam\\steamapps\\common", pushTheButton)
+  .run()
 
-const LOCALIZATION = {}
-
-const shuffle = () => {
-  return _(LOCALIZATION)
-    .map((value, key) => ({ [key]: value }))
-    .shuffle()
-    .reduce((one, another) => ({ ...one, ...another }), {})
-}
-
-
-
-export const replaceWithIdIfPrompt = (field) => {
-
-  if(!isPrompt(field.n))
-    return field;
-  // ALL OF THEM HAVE "v" BUT SOME HAVE "s".
-  // WHEN "s" IS PRESENT WE DONT CARE ABOUT "v"
-  // SO GETTING THE FIRST ONE SHOULD BE ENOUGH
-  const property = _.find(TEXT_PROPERTIES, (property) => _.has(field, property));
-  const id = `TRANSLATION_${hash(field[property])}`;
-
-  // HAVING EFFECT INSIDE MAP => NOT COOL
-   _.assign(LOCALIZATION, { [id]: field[property] })
-  return { ...field, [property]: id}
-}
-
-
-export const execute = (folder) => {
-    processFolder(folder, replaceWithIdIfPrompt)
-    .then(() => shuffle(LOCALIZATION)) 
-    .then(localization => fs.writeFileAsync("./localization.json", JSON.stringify(localization)))
-}

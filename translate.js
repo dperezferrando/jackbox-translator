001c587ede4f7164b4794c192b27cae2283a6d70
Diego <diegoperezferrando@gmail.com>
2020-03-24 23:20:58 -0300

    Quiplas 2 translation

--- translate.js
+++ translate.js
@@ -1,5 +1,6 @@
 import JackboxTranslator from "./domain/jackboxTranslator";
 import quiplash2 from "./domain/games/quiplash2"
+import pushTheButton from "./domain/games/pushTheButton"
 import * as localization from "./localization"
 
 new JackboxTranslator("I:\Juegos\\Steam\\steamapps\\common", quiplash2, localization)

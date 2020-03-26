cc3a68a8ae5efcdf660ef6389684feda77ccb459
Diego <diegoperezferrando@gmail.com>
2020-03-25 21:27:32 -0300

    Update comment

--- domain/games/pushTheButton.js
+++ domain/games/pushTheButton.js
@@ -1,8 +1,8 @@
 export default {
   path: "The Jackbox Party Pack 6\\games\\PushTheButton\\content",
   dataFiles: ["PushTheButtonDrawingTests", "PushTheButtonMoralityTests", "PushTheButtonRatingTests", "PushTheButtonWritingTests", "data.jet"],
-  textProperties: ["v"], // THE PROPERTIES THAT SHOULD BE TRANSLATED ORDER IS IMPORTANT
+  textProperties: ["v"], // THE PROPERTIES THAT SHOULD BE TRANSLATED
   promptIdProperty: ["n"], // THE PROPERY THAT IDENTIFIES THAT OBJECT AS A POSSIBLE PROMPT TEXT
-  promptsRegExp: ["Text", "HumanPromptAudio"], // THE STRINGS THAT WILL BE USED AS REGEXP TO IDENTIFY A PROMPT USING promptIdProperty
+  promptsRegExp: ["Text"], // THE STRINGS THAT WILL BE USED AS REGEXP TO IDENTIFY A PROMPT USING promptIdProperty
   arrayName: "fields"
 }

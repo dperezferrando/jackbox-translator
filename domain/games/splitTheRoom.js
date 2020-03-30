ee9d132e973e836cf26e22c072ecc38c0fc2bd6a
Diego <diegoperezferrando@gmail.com>
2020-03-30 18:38:37 -0300

    Fix regexp

--- domain/games/splitTheRoom.js
+++ domain/games/splitTheRoom.js
@@ -3,6 +3,6 @@ export default {
   dataFiles: ["SplitTheRoomFinal", "SplitTheRoomLater", "SplitTheRoomShortie", "SplitTheRoomFinal.jet", "SplitTheRoomLater.jet", "SplitTheRoomShortie.jet", "data.jet"],
   textProperties: ["answerText", "category", "decoys", "questionText", "scenarioText", "v"], // THE PROPERTIES THAT SHOULD BE TRANSLATED
   promptIdProperty: ["n"], // THE PROPERY THAT IDENTIFIES THAT OBJECT AS A POSSIBLE PROMPT TEXT
-  promptsRegExp: ["ScenarioText", "QuestionText", "Decoys", "Answer"], // THE STRINGS THAT WILL BE USED AS REGEXP TO IDENTIFY A PROMPT USING promptIdProperty
+  promptsRegExp: ["ScenarioText[0-9]?$", "QuestionText[0-9]?$", "Decoys", "Answer"], // THE STRINGS THAT WILL BE USED AS REGEXP TO IDENTIFY A PROMPT USING promptIdProperty
   arrayName: "content" // THE NAME OF THE ARRAY THAT CONTAINS EVERY OBJECT IN THE DATAFILES
 }

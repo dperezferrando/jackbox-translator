cab8436e3ba13524272f77770e75db721d201dba
Diego <diegoperezferrando@gmail.com>
2020-04-16 23:42:41 -0300

    Games mapper for creating Game objects

---
+++ domain/games/gamesMapper.js
@@ -0,0 +1,17 @@
+import quiplash2 from "./quiplash2"
+import pushTheButton from "./pushTheButton"
+import patentlyStupid from "./patentlyStupid"
+import quiplash from "./quiplash"
+import jokeBoat from "./jokeBoat"
+import murderParty2 from "./murderParty2"
+import roleModels from "./roleModels"
+
+export default {
+  quiplash2,
+  pushTheButton,
+  patentlyStupid,
+  quiplash,
+  jokeBoat,
+  murderParty2,
+  roleModels
+};
